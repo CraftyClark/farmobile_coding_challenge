ea4b5826c41b5164cde095b04af805265238e265
Andrew Clark <andrewclark.ku@gmail.com>
2020-02-09 14:25:37 -0600

    added total runtime functionality

--- venv/Scripts/app.py
+++ venv/Scripts/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 import csv
-from collections import Counter
+import time
+from datetime import datetime, timedelta
 
-filename = '../Include/gps_small_can_dataset.csv'
+filename = '../Include/custom_data.csv'
+
+starttime = time.time()
 
 can_messages_dict = {}
 
@@ -20,6 +23,12 @@ def returnCanCount():
 def returnUniqueCanCount():
     return str(unique_can_messages_count)
 
+@app.route('/total-runtime')
+def returnTotalRuntime():
+    total_runtime_object = latest_timestamp - earliest_timestamp
+    total_runtime = total_runtime_object.days * 24 * 3600 + total_runtime_object.seconds
+    return str(total_runtime)
+
 
 def totalUniqueCanMessages(id, unique_can_messages_count):
     if id in can_messages_dict:
@@ -32,6 +41,13 @@ def totalUniqueCanMessages(id, unique_can_messages_count):
     return unique_can_messages_count
 
 
+def convertDateToDateObject(date_string):
+    # save date string into date object: current_row_date
+    current_row_date = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
+    return current_row_date
+
+
+
 def lookAtData():
 
     global gps_messages_count 
@@ -40,12 +56,27 @@ def lookAtData():
     can_messages_count = 0
     global unique_can_messages_count
     unique_can_messages_count = 0
-
+    # initialize earliest timestamp using a far future date
+    global earliest_timestamp
+    earliest_timestamp = datetime(3000, 1, 1)
+    # initialize latest timestamp using a far past date
+    global latest_timestamp
+    latest_timestamp = datetime(1000, 1, 1)
 
     with open(filename) as csvfile:  
         data = csv.DictReader(csvfile)
         for row in data:
             
+            # handle total runtime of data in file
+            # get date object from date string
+            current_row_date = convertDateToDateObject(row['ts'])
+            # check if current date is the earliest or latest 
+            if current_row_date < earliest_timestamp:
+                earliest_timestamp = current_row_date
+            elif current_row_date > latest_timestamp:
+                latest_timestamp = current_row_date
+
+
             current_gps_id = row['gps_id']
             current_message_id = row['message_id']
 
@@ -63,6 +94,9 @@ def lookAtData():
 
 
 
+
 if __name__ == '__main__':
     lookAtData()
     app.run(debug=True)
+
+    print("Program run time = {} seconds".format(time.time() - starttime))
